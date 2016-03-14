a6ca7a5ba2d6ae782b1944210d044201b83d590b
Max <baby624mills@mail.ru>
2016-03-14 17:50:26 +0200

    j

---
+++ src/Java_Lang/Appe.java
@@ -0,0 +1,14 @@
+package Java_Lang;
+
+/**
+ * Created by PC on 14.03.2016.
+ */
+public class Appe {
+    public static void main(String[] args) {
+        String s = "This text";
+        String upper = s.toUpperCase();
+        String lower = s.toLowerCase();
+        System.out.println(upper);
+        System.out.println(lower);
+    }
+}

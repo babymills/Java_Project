e6f3e0b80ec52d5247aa37afa49a996cb6abc381
Max <baby624mills@mail.ru>
2016-03-14 18:31:58 +0200

    j

--- src/Java_Lang/Appe.java
+++ src/Java_Lang/Appe.java
@@ -1,14 +1,40 @@
 package Java_Lang;
 
+import lesons.S;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 /**
  * Created by PC on 14.03.2016.
  */
 public class Appe {
-    public static void main(String[] args) {
-        String s = "This text";
+    public static void main(String[] args) throws IOException {
+      /*  String s = "This text";
         String upper = s.toUpperCase();
         String lower = s.toLowerCase();
         System.out.println(upper);
         System.out.println(lower);
+        String c = "";
+        boolean v = c.isEmpty();
+        System.out.println(v);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> list = new ArrayList<>();
+        for(int i  = 0;i<5;i++) {
+            String sqw = reader.readLine();
+          list.add(sqw);
+            while (sqw.isEmpty()) {
+                break;
+            }
+        }
+         for(String q:list){
+             System.out.println(q);
+         }*/
+        //---------------------------------------
+
+        }
     }
-}
+

019c0ef006a0e1c09692dfcd5e4aaa61d8fe5c70
Max <baby624mills@mail.ru>
2016-03-14 17:26:30 +0200

    j

---
+++ src/Java_Lang/Ap001.java
@@ -0,0 +1,14 @@
+package Java_Lang;
+
+/**
+ * Created by PC on 14.03.2016.
+ */
+public class Ap001 {
+    public static void main(String[] args) {
+        /*String s = "one";
+        String s1 = s.concat("two");
+        String s2 = "Hello".replace("l","w");
+        String s3 = "     Hello world  ".trim();
+---------------------------------------------------------*/
+    }
+}

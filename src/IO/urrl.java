5bb252c7a809b956f06fa089e17f6fe78b26c5dc
babymills <baby624mills@mail.ru>
2016-03-25 19:57:23 +0200

    jj

---
+++ src/IO/urrl.java
@@ -0,0 +1,29 @@
+package IO;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by привет on 25.03.2016.
+ */
+public class urrl {
+    public static void main(String[] args) throws IOException {
+
+        InputStream src = new URL("http://google.ru").openStream();
+      //  OutputStream dsc = new java.io.FileOutputStream("c:\\idea\\g.txt");
+       // copy(src,dsc);//в файл
+          copy(src,System.out);//на консоль
+    }
+    public  static void copy(InputStream src, OutputStream dsc) throws IOException {
+        while (true){
+            int data = src.read();
+            if(data !=-1){
+                dsc.write(data);
+            }else return;
+        }
+    }
+}

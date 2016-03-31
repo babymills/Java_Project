d3596a7c685d1d395df220e5be5ff3a6c0db3f28
Max <baby624mills@mail.ru>
2016-04-01 00:32:24 +0300

    j

--- src/IO/DataIODemo.java
+++ src/IO/DataIODemo.java
@@ -3,6 +3,7 @@ package IO;
 import javafx.scene.Parent;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -12,8 +13,17 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 
     class DataIODemo  {
-    public static void main(String[] args) {
-       int a[] = new int []{1, 2, 3, 4, 5};
+    public static void main(String[] args) throws Throwable {
+         String str = new String(new byte [] {0,1,2});
+        String c = "A";
+        char cc[]= {2};
+        System.out.println(str);
+        System.out.println( );
+        System.out.println("A".getBytes("UTF-16").length);//4
+        System.out.println("AA".getBytes("UTF-16").length);//6
+        System.out.println("AAA".getBytes("UTF-16").length);//6
+        System.out.println("A".getBytes("UTF-8").length);//1
+        System.out.println("AA".getBytes("UTF-8").length);//2
     }
 }
 
